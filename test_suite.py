02b009b92e1007cf54ebe651fb4c17a978b4f35d
Manuel Vidaurre <manuel.vidaurre@benandfrank.com>
2026-01-19 13:24:56 -0600

    Fix indentation errors in test_suite.py
    - Fixed print statement indentations in _run_tests function
    - Removed obsolete 'fix_print_with_import' comment markers
    - All print statements now properly indented within function scope
    - Resolves remaining GitHub Actions test workflow failures

--- test_suite.py
+++ test_suite.py
@@ -10,6 +10,7 @@ Contact : etienne at kartoza dot com
      (at your option) any later version.
 
 """
+
 from __future__ import print_function
 
 import sys
@@ -21,34 +22,26 @@ from os import listdir
 
 from osgeo import gdal
 
-__author__ = 'etiennetrimaille'
-__revision__ = '$Format:%H$'
-__date__ = '14/06/2016'
+__author__ = "etiennetrimaille"
+__revision__ = "$Format:%H$"
+__date__ = "14/06/2016"
 
 
 def _run_tests(test_suite, package_name):
     """Core function to test a test suite."""
     count = test_suite.countTestCases()
-    # fix_print_with_import
-    # fix_print_with_import
-print('########')
-    # fix_print_with_import
-    # fix_print_with_import
-print('%s tests has been discovered in %s' % (count, package_name))
-    # fix_print_with_import
-    # fix_print_with_import
-print('Python GDAL : %s' % gdal.VersionInfo('VERSION_NUM'))
-    # fix_print_with_import
-    # fix_print_with_import
-print('########')
+    print("########")
+    print("%s tests has been discovered in %s" % (count, package_name))
+    print("Python GDAL : %s" % gdal.VersionInfo("VERSION_NUM"))
+    print("########")
     currentdir = dirname(abspath(inspect.getfile(inspect.currentframe())))
     parentdir = dirname(currentdir)
     sys.path.insert(0, parentdir)
-    sys.path.insert(0, '/root/.qgis2/python/plugins/GeoPublicHealth')
+    sys.path.insert(0, "/root/.qgis2/python/plugins/GeoPublicHealth")
     unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
 
 
-def test_package(package='src'):
+def test_package(package="src"):
     """Test package.
     This function is called by travis without arguments.
 
@@ -63,5 +56,5 @@ def test_package(package='src'):
     _run_tests(test_suite, package)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_package()
