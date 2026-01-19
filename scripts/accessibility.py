6feca13e6aec2cbdb47dc6ded8f2a93a4d5070aa
Manuel Vidaurre <manuel.vidaurre@benandfrank.com>
2026-01-19 13:23:57 -0600

    Fix indentation errors in scripts/accessibility.py
    - Fixed print statement indentation at line 69 in create_grid function
    - Fixed print statement indentation at line 237 in assign_cost_to_cells function
    - Removed obsolete 'fix_print_with_import' comment markers
    - Resolves GitHub Actions test workflow failure

--- scripts/accessibility.py
+++ scripts/accessibility.py
@@ -25,14 +25,13 @@ from GeoPublicHealth.src.core.accessibility.network import Graph
 
 import processing
 
-path = '/Users/etienne/dev/python/GeoPublicHealth/src/test/data'
-roads = QgsVectorLayer(join(path, 'roads.geojson'), 'roads', 'ogr')
+path = "/Users/etienne/dev/python/GeoPublicHealth/src/test/data"
+roads = QgsVectorLayer(join(path, "roads.geojson"), "roads", "ogr")
 extent = QgsRectangle(335147, 6636000, 352384, 6650000)
 cell_size = 5000
-health_points = QgsVectorLayer(
-    join(path, 'crossing.geojson'), 'health_points', 'ogr')
+health_points = QgsVectorLayer(join(path, "crossing.geojson"), "health_points", "ogr")
 number_of_points = 3
-id_field = 'osm_id'
+id_field = "osm_id"
 
 
 def create_grid(size):
@@ -44,29 +43,29 @@ def create_grid(size):
     :return: The grid layer in memory.
     :rtype: QgsVectorLayer
     """
-    output_filename = unique_filename(prefix='grid', suffix='.shp')
+    output_filename = unique_filename(prefix="grid", suffix=".shp")
 
     result = processing.runalg(
-        'qgis:vectorgrid',
-        '336199.970553,352338.397991,7636164.67975,7648562.41208',
+        "qgis:vectorgrid",
+        "336199.970553,352338.397991,7636164.67975,7648562.41208",
         size,  # X spacing
         size,  # Y spacing
         0,  # Output as polygons
-        output_filename)
+        output_filename,
+    )
 
-    layer = QgsVectorLayer(output_filename, 'grid', 'ogr')
+    layer = QgsVectorLayer(output_filename, "grid", "ogr")
     layer.setCrs(QgsCoordinateReferenceSystem(32740))
 
-    remove_fields(layer, ['xmin', 'xmax', 'ymin', 'ymax'])
+    remove_fields(layer, ["xmin", "xmax", "ymin", "ymax"])
 
     # Make a copy in memory
     memory = create_memory_layer(
-        'grid', layer.geometryType(), layer.crs(), layer.fields())
+        "grid", layer.geometryType(), layer.crs(), layer.fields()
+    )
     copy_layer(layer, memory)
 
-    # fix_print_with_import
-    # fix_print_with_import
-print("NB cells : %s" % layer.featureCount())
+    print("NB cells : %s" % layer.featureCount())
 
     return memory
 
@@ -75,9 +74,8 @@ def intersect_grid_and_roads(grid, roads):
     """Create a subset of the grid."""
 
     grid.startEditing()
-    grid.addAttribute(
-        QgsField('has_road', QVariant.String, len=10, prec=0))
-    intersection_index = grid.fields().indexFromName('has_road')
+    grid.addAttribute(QgsField("has_road", QVariant.String, len=10, prec=0))
+    intersection_index = grid.fields().indexFromName("has_road")
 
     spatial_index = create_spatial_index(roads)
 
@@ -96,8 +94,7 @@ def intersect_grid_and_roads(grid, roads):
         else:
             has_intersection = False
 
-        grid.changeAttributeValue(
-            cell.id(), intersection_index, has_intersection)
+        grid.changeAttributeValue(cell.id(), intersection_index, has_intersection)
 
     grid.commitChanges()
     return grid
@@ -106,10 +103,8 @@ def intersect_grid_and_roads(grid, roads):
 def centroids(layer):
     """Create centroids."""
     centroids_layer = create_memory_layer(
-        'centroids',
-        Qgis.Point,
-        layer.crs(),
-        layer.fields())
+        "centroids", Qgis.Point, layer.crs(), layer.fields()
+    )
 
     centroids_layer.startEditing()
 
@@ -135,7 +130,7 @@ def create_graph(network, centroid_layer, target_point_layer):
 
 def intersecting_blocks(line, destination, grid):
     """Function to fetch intersectings polygons from the grid."""
-    dest_id_field = grid.fields().indexFromName('destination_id')
+    dest_id_field = grid.fields().indexFromName("destination_id")
     request = QgsFeatureRequest()
     request.setFilterRect(line.boundingBox())
     request.setFilterExpression('"destination_id" is None')
@@ -165,43 +160,40 @@ def assign_cost_to_cells(network_graph, source, destination, id_field):
         destination_features[feature.id()] = feature
     index_id_field = destination.fields().indexFromName(id_field)
 
-    fields = [QgsField('distance', QVariant.Int)]
-    routes = create_memory_layer(
-        'routes', Qgis.Line, destination.crs(), fields)
+    fields = [QgsField("distance", QVariant.Int)]
+    routes = create_memory_layer("routes", Qgis.Line, destination.crs(), fields)
     routes.startEditing()
 
     source.startEditing()
-    source.addAttribute(
-        QgsField('destination_id', QVariant.Int, len=5, prec=0))
-    dest_id_field = source.fields().indexFromName('destination_id')
-    source.addAttribute(QgsField('distance', QVariant.Int, len=5, prec=0))
-    distance_field = source.fields().indexFromName('distance')
+    source.addAttribute(QgsField("destination_id", QVariant.Int, len=5, prec=0))
+    dest_id_field = source.fields().indexFromName("destination_id")
+    source.addAttribute(QgsField("distance", QVariant.Int, len=5, prec=0))
+    distance_field = source.fields().indexFromName("distance")
     source.commitChanges()
 
     request = QgsFeatureRequest()
-    request.setFilterExpression('"has_road" = \'true\'')
+    request.setFilterExpression("\"has_road\" = 'true'")
     # request.setLimit(20)  # Hack for now to speedup development
     i = 0
     for source_cell in source.getFeatures(request):
         source_geometry_point = source_cell.geometry().centroid().asPoint()
-        desination_id = source_cell['destination_id']
+        desination_id = source_cell["destination_id"]
         if desination_id is None or isinstance(desination_id, QPyNullVariant):
             source.startEditing()
             nearest_health_points = spatial_index.nearestNeighbor(
-                source_geometry_point, 5)
+                source_geometry_point, 5
+            )
             minimum_distance = None
             minimal_geom = None
             for health_point in nearest_health_points:
                 try:
                     i += 1
                     p = destination_features[health_point].geometry().asPoint()
-                    geom, distance, _ = network_graph.route(
-                        source_geometry_point, p)
+                    geom, distance, _ = network_graph.route(source_geometry_point, p)
                 except:
                     distance = -1
 
-                if minimum_distance is None or (
-                                minimum_distance > distance >= 0):
+                if minimum_distance is None or (minimum_distance > distance >= 0):
                     minimal_geom = geom
                     minimum_distance = distance
 
@@ -214,30 +206,32 @@ def assign_cost_to_cells(network_graph, source, destination, id_field):
                 index_id = destination_features[health_point][index_id_field]
                 destination_value = index_id
             else:
-                destination_value = '-1'
-                minimum_distance = '-1'
+                destination_value = "-1"
+                minimum_distance = "-1"
 
             source.changeAttributeValue(
-                source_cell.id(), dest_id_field, destination_value)
+                source_cell.id(), dest_id_field, destination_value
+            )
             source.changeAttributeValue(
-                source_cell.id(), distance_field, minimum_distance)
+                source_cell.id(), distance_field, minimum_distance
+            )
             intersecting_blocks(minimal_geom, destination_value, grid)
             source.commitChanges()
 
         else:
             # fix_print_with_import
-            print('speedup')
+            print("speedup")
             geom, distance, _ = network_graph.route(
                 source_geometry_point,
-                destination_features[desination_id].geometry().asPoint())
+                destination_features[desination_id].geometry().asPoint(),
+            )
 
     source.commitChanges()
     # source.commitErrors()
     routes.commitChanges()
-    # fix_print_with_import
-    # fix_print_with_import
-print('Call : %s' % i)
+    print("Call : %s" % i)
     return routes, source
+
 
 if health_points.crs != roads.crs():
     health_points = reproject(health_points, roads.crs())
